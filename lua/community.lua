d0c6c94bf3d7390c3de9afffcb1b49b40f81626e
ZAAFHachemrachid <rachidzaaf9@gmail.com>
2024-07-18 12:03:54 +0100

    hello

---
+++ lua/community.lua
@@ -0,0 +1,16 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.svelte" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  -- import/override with your plugins folder
+}
